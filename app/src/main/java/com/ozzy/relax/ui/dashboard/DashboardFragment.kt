63ae1da1ba3690bee2ca28fdd5803b8bb5e069bb
Oğuzhan <Oguzhan.Karacan@vodafone.com>
2021-09-04 02:02:07 +0300

    added viewmodel to dashboard fragment

--- app/src/main/java/com/ozzy/relax/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/ozzy/relax/ui/dashboard/DashboardFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.ozzy.relax.R
 import com.ozzy.relax.databinding.DashboardFragmentBinding
@@ -12,6 +13,7 @@ import com.ozzy.relax.databinding.DashboardFragmentBinding
 class DashboardFragment : Fragment() {
 
     lateinit var binding: DashboardFragmentBinding
+    val viewModel: DashboardViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
